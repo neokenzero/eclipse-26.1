47ab374f911f116a6894d5da9d93cc488c59ebce
Kub3s-d <syckovaksenia@gmail.com>
2026-07-04 07:38:07 +0700

    Initial commit

---
+++ src/client/java/kub3s/eclipse/client/EclipseClient.java
@@ -0,0 +1,10 @@
+package kub3s.eclipse.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EclipseClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/kub3s/eclipse/client/EclipseDataGenerator.java
@@ -0,0 +1,11 @@
+package kub3s.eclipse.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EclipseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
